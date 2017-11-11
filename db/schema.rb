bf4ec0fccfa9ada942313755a7daac42dbec6bd5
Pablo Munoz <pfariasmunoz@gmail.com>
2017-11-11 11:57:52 -0300

    Add postgres check command to emails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110172317) do
+ActiveRecord::Schema.define(version: 20171111141207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
